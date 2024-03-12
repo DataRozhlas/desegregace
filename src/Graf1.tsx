f2184af20cd17804902a9de0d842eb40665c79a0
Petr Kočí <petr.koci@samizdat.cz>
2024-03-12 14:29:20 +0100

    graf 2

--- src/Graf1.tsx
+++ src/Graf1.tsx
@@ -1,7 +1,7 @@
 import Highcharts from 'highcharts';
 import SeriesLabel from 'highcharts/modules/series-label';
 import {
-  HighchartsProvider, HighchartsChart, Chart, XAxis, YAxis, LineSeries
+  HighchartsProvider, HighchartsChart, XAxis, YAxis, LineSeries, Tooltip
 } from 'react-jsx-highcharts';
 
 const plotOptions = {
@@ -24,12 +24,13 @@ export default function Graf1() {
 
         <HighchartsChart plotOptions={plotOptions}>
           <XAxis />
-          <YAxis labels={{ enabled: true, formatter: function () { return this.isLast ? this.value + " %" : this.value + "" } }}>
+          <YAxis labels={{ enabled: true, formatter: function () { return this.isLast ? this.value + " %" : this.isFirst ? "" : this.value + "" } }}>
             <YAxis.Title>Podíl romských žáků</YAxis.Title>
             <LineSeries name="Mateřské školy" data={[1.9, 2.1, 1.9, 1.9, 1.8, 1.9]} color="#fd8d3c" dataLabels={{ enabled: true, color: "#fd8d3c", style: { textOutline: "none", fontSize: "0.8rem" }, formatter: function () { return this.point.index + 1 === this.series.data.length ? this.y?.toLocaleString("cs-CZ") + " %" : null } }} />
             <LineSeries name="Základní školy" data={[3.6, 3.7, 3.5, 3.6, 3.6, 3.5]} color="#e6550d" dataLabels={{ enabled: true, color: "#e6550d", style: { textOutline: "none", fontSize: "0.8rem" }, formatter: function () { return this.point.index + 1 === this.series.data.length ? this.y?.toLocaleString("cs-CZ") + " %" : null } }} />
             <LineSeries name="Střední školy" data={[1.3, 1.2, 1.2, 1.1, 1.1, 1.2]} color="#a63603" dataLabels={{ enabled: true, color: "#a63603", style: { textOutline: "none", fontSize: "0.8rem" }, formatter: function () { return this.point.index + 1 === this.series.data.length ? this.y?.toLocaleString("cs-CZ") + " %" : null } }} />
           </YAxis>
+          <Tooltip valueSuffix=' %' shared={true} />
         </HighchartsChart>
       </HighchartsProvider>
     </div>
--- src/Graf2.tsx
+++ src/Graf2.tsx
@@ -1,8 +1,46 @@
+import Highcharts from 'highcharts';
+import {
+    HighchartsProvider, HighchartsChart, Chart, Legend, XAxis, YAxis, BarSeries, Tooltip
+} from 'react-jsx-highcharts';
+
+
+
 export default function Graf2() {
 
     return (
         <div className="max-w-[620px] mx-auto">
-            graf2
+            <h1 className="text-xl font-bold mb-1">Na 129 základních školách je Romů víc než třetina...</h1>
+            <h2 className="mb-3">Údaje za školní rok 2021/22</h2>
+
+            <HighchartsProvider Highcharts={Highcharts}>
+
+                <HighchartsChart plotOptions={{}}>
+                    <Chart type="bar" marginRight={20} marginLeft={2} height={200} />
+                    <Legend verticalAlign='top' />
+                    <XAxis categories={[""]} />
+                    <YAxis reversedStacks={false} labels={{ formatter: function () { return this.isLast ? this.value + " %" : this.value.toString() } }}>
+                        <BarSeries name="bez romských žáků" data={[2381]} stacking="percent" color="#f2d0a2" />
+                        <BarSeries name="s romskými žáky" data={[1739]} stacking='percent' color="#85a1e0" />
+                        <BarSeries name="s více než třetinou romských žáků" data={[129]} stacking='percent' color="#5651ce" />
+                    </YAxis>
+                    <Tooltip valueSuffix=' škol' />
+                </HighchartsChart>
+                <h1 className="text-xl font-bold mt-7 mb-1">... na stovce škol víc než polovina</h1>
+
+                <HighchartsChart plotOptions={{}}>
+                    <Chart type="bar" marginRight={20} marginLeft={2} height={200} />
+                    <Legend verticalAlign='top' reversed={true} />
+                    <XAxis categories={[""]} />
+                    <YAxis reversedStacks={true} labels={{ formatter: function () { return this.isLast ? this.value + " škol" : this.value.toString() } }}>
+                        <BarSeries name="33-50 % Romů" data={[29]} stacking="normal" color="#92b8dd" />
+                        <BarSeries name="51-75 % Romů" data={[69]} stacking='normal' color="#6685d1" />
+                        <BarSeries name="76 % a více Romů " data={[31]} stacking='normal' color="#4850bc" />
+                    </YAxis>
+                    <Tooltip valueSuffix=' škol' />
+                </HighchartsChart>
+
+            </HighchartsProvider>
+
         </div>
     )
 }
